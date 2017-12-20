b7640e921de8a1b6b8f957d1fdba99f81808049b
liyazhou <liyazhou@outlook.com>
2017-12-20 18:19:43 +0800

    add gitignore pom

--- Chapter1/src/main/java/com/didispace/web/HelloController.java
+++ Chapter1/src/main/java/com/didispace/web/HelloController.java
@@ -3,12 +3,21 @@ package com.didispace.web;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 public class HelloController {
 
+//    http://127.0.0.1:8080/hello
     @RequestMapping("/hello")
     public String index() {
         return "Hello World";
     }
-
+    //    http://127.0.0.1:8080/test
+    @RequestMapping("/test")
+    public String test(HttpServletRequest request, HttpServletResponse response) {
+        System.out.println(request.getRequestURL());
+        return "test";
+    }
 }
