2638433399a9513bd01bafb41e936f74340490a8
liyazhou <liyazhou@outlook.com>
2017-12-21 21:34:02 +0800

    learning, jpa, thymeleaf

--- Chapter3-1-4/src/main/java/com/didispace/web/HelloController.java
+++ Chapter3-1-4/src/main/java/com/didispace/web/HelloController.java
@@ -3,7 +3,8 @@ package com.didispace.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
@@ -19,12 +20,18 @@ public class HelloController {
     @ResponseBody
     public String hello() {
         return "Hello World";
-    }
 
+    }
     @RequestMapping("/")
     public String index(ModelMap map) {
         map.addAttribute("host", "http://blog.didispace.com");
         return "index";
+    }
+    @RequestMapping("/test")
+    public ModelAndView test(ModelMap map) {
+        ModelAndView modelAndView = new ModelAndView("index");
+        map.addAttribute("host", "http://blog.didispace.com");
+        return modelAndView;
     }
 
 }
