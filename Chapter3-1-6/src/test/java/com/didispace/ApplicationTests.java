6f622c5f707d52138810ea2c45dc6f9a260fc472
liyazhou <liyazhou@outlook.com>
2018-01-16 16:32:36 +0800

    统一异常处理

--- Chapter3-1-6/src/test/java/com/didispace/ApplicationTests.java
+++ Chapter3-1-6/src/test/java/com/didispace/ApplicationTests.java
@@ -1,5 +1,6 @@
 package com.didispace;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -17,6 +18,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 //@WebAppConfiguration
 public class ApplicationTests {
 
+    @Test
+    public void test() {
 
+    }
 
 }
