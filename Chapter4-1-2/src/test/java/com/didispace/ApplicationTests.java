32ec7dd6cbf77d3df053e05ae7d2e94265f2a710
liyazhou <liyazhou@outlook.com>
2018-01-17 11:39:18 +0800

    async

--- Chapter4-1-2/src/test/java/com/didispace/ApplicationTests.java
+++ Chapter4-1-2/src/test/java/com/didispace/ApplicationTests.java
@@ -4,40 +4,39 @@ import com.didispace.async.Task;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.concurrent.Future;
 
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ApplicationTests {
 
-	@Autowired
-	private Task task;
+    @Autowired
+    private Task task;
 
-	@Test
-	public void test() throws Exception {
+    @Test
+    public void test() throws Exception {
 
-		long start = System.currentTimeMillis();
+        long start = System.currentTimeMillis();
 
-		Future<String> task1 = task.doTaskOne();
-		Future<String> task2 = task.doTaskTwo();
-		Future<String> task3 = task.doTaskThree();
+        Future<String> task1 = task.doTaskOne();
+        Future<String> task2 = task.doTaskTwo();
+        Future<String> task3 = task.doTaskThree();
+        while (true) {
+            if (task1.isDone() && task2.isDone() && task3.isDone()) {
+                // 三个任务都调用完成，退出循环等待
+                break;
+            }
+            Thread.sleep(1000);
+        }
 
-		while(true) {
-			if(task1.isDone() && task2.isDone() && task3.isDone()) {
-				// 三个任务都调用完成，退出循环等待
-				break;
-			}
-			Thread.sleep(1000);
-		}
+        long end = System.currentTimeMillis();
 
-		long end = System.currentTimeMillis();
+        System.out.println("任务全部完成，总耗时：" + (end - start) + "毫秒");
 
-		System.out.println("任务全部完成，总耗时：" + (end - start) + "毫秒");
-
-	}
+    }
 
 }
